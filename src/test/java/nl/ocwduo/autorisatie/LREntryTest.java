47f5e6c485f4e6d17817b712a8a52b5229e72cca
Arjen <arjen@bax.frl>
2017-11-10 21:14:02 +0100

    testroutines toegevoegd voor LREntry

--- src/test/java/nl/ocwduo/autorisatie/LREntryTest.java
+++ src/test/java/nl/ocwduo/autorisatie/LREntryTest.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.ocwduo.autorisatie;
 
+import org.easymock.EasyMock;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -13,12 +10,45 @@ import org.junit.Test;
  */
 public class LREntryTest
 {
+    static final String TESTKEY1 = "Bax:Arjen";
+    static final String TESTKEY2 = "Bax:Arjen";
 
-    public LREntryTest() {
+    @Test
+    public void testCreateLeftEntry() {
+        SyncableObject mockLeft = EasyMock.mock(SyncableObject.class);
+        EasyMock.expect(mockLeft.getKey()).andReturn(TESTKEY1).anyTimes();
+        SyncableObject mockRight = EasyMock.mock(SyncableObject.class);
+        EasyMock.expect(mockRight.getKey()).andReturn(TESTKEY2).anyTimes();
+
+        EasyMock.replay(mockLeft, mockRight);
+
+        LREntry testEntry = LREntry.createLeftEntry(mockLeft);
+        assertSame(mockLeft, testEntry.getLeftItem());
+        assertNull(testEntry.getRightItem());
+        assertEquals(TESTKEY1, testEntry.getKey());
+
+        testEntry.addRightItem(mockRight);
+        assertSame(mockRight, testEntry.getRightItem());
+        assertEquals(TESTKEY2, mockRight.getKey());
+
+        try {
+            testEntry.addRightItem(mockRight);
+            fail("should never arrive here");
+        } catch (Exception ex) {
+            assertTrue(ex instanceof IllegalStateException);
+        }
     }
 
     @Test
-    public void testSomeMethod() {
-    }
+    public void testCreateRightEntry() {
+        SyncableObject mockRight = EasyMock.mock(SyncableObject.class);
+        EasyMock.expect(mockRight.getKey()).andReturn(TESTKEY2).anyTimes();
 
+        EasyMock.replay(mockRight);
+
+        LREntry testEntry = LREntry.createRightEntry(mockRight);
+        assertSame(mockRight, testEntry.getRightItem());
+        assertNull(testEntry.getLeftItem());
+        assertEquals(TESTKEY2, testEntry.getKey());
+    }
 }
